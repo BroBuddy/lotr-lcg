38749b98533da2d173598efba72fa53e4458feb3
Sebastian <sebastian.stein@impect.com>
2021-01-22 17:15:59 +0100

    fixed bug

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -79,6 +79,7 @@ export class DataService {
     this.activeLocation.next(scenario[0].activeLocation);
     this.stagingArea.next(scenario[0].stagingArea);
     this.discardPile.next(scenario[0].discardPile);
+    this.engagingArea.next(null);
     this.questDeck.next(scenario[0].questDeck);
 
     if (shuffle) {

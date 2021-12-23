86cf02c2bec74660c7835377a4fa42d2eb81ff33
Sebastian <sebastian.stein@impect.com>
2021-12-23 12:12:28 +0100

    fixed speech + adding comands

--- src/app/scenario/scenario.component.ts
+++ src/app/scenario/scenario.component.ts
@@ -1,13 +1,13 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Params, Router} from "@angular/router";
-import {ToastrService} from "ngx-toastr";
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 import Speech from 'speak-tts';
-import {SubSink} from "subsink";
+import {SubSink} from 'subsink';
 
 import {Observable} from 'rxjs';
 
 import {DataService} from '../data/data.service';
-import {ImageZoomService} from "../image-zoom/image-zoom.service";
+import {ImageZoomService} from '../image-zoom/image-zoom.service';
 
 @Component({
   selector: 'app-scenario',
@@ -19,16 +19,46 @@ export class ScenarioComponent implements OnInit, OnDestroy {
   public text: string;
   public scenario$: Observable<any>;
   private subs = new SubSink();
-  public speech: any;
+  public speech = new Speech();
   public speechSupport: boolean;
   public speechConfig = {
     volume: 1,
     lang: 'en-GB',
     rate: 1,
     pitch: 1,
-    voice: 'Daniel',
-    splitSentences: false
+    voice: 'Google UK English Male',
+    splitSentences: true
   };
+  public commandList = [
+    {
+      icon: 'subdirectory_arrow_left',
+      content: 'Draw another encounter card'
+    },
+    {
+      icon: 'arrow_back',
+      content: 'Previous quest card'
+    },
+    {
+      icon: 'arrow_forward',
+      content: 'Next quest card'
+    },
+    {
+      icon: 'arrow_upward',
+      content: 'Increase quest threat'
+    },
+    {
+      icon: 'arrow_downward',
+      content: 'Decrease quest threat'
+    },
+    {
+      icon: 'add_circle_outline',
+      content: 'Increase players threat'
+    },
+    {
+      icon: 'remove_circle_outline',
+      content: 'Decrease players threat'
+    }
+  ];
 
   constructor(private route: ActivatedRoute,
               private router: Router,
@@ -52,8 +82,6 @@ export class ScenarioComponent implements OnInit, OnDestroy {
   }
 
   initSpeech(): void {
-    this.speech = new Speech();
-
     if (this.speech.hasBrowserSupport()) {
       this.speechSupport = true;
       this.speech.init(this.speechConfig);
@@ -64,6 +92,7 @@ export class ScenarioComponent implements OnInit, OnDestroy {
   }
 
   onStartReading(): void {
+    this.speech.resume();
     this.speech.speak({ text: this.text });
   }
 

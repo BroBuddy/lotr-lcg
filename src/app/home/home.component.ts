3c108c98628e3d4ba952dd18ce4083cc72a31fa8
Sebastian Stein <sebastian.stein@denkwerk.com>
2023-03-24 16:22:47 +0100

    added card amount

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import {Observable} from 'rxjs';
+import { Observable } from "rxjs";
 
-import {DataService} from '../data/data.service';
+import { DataService } from "../data/data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html'
+  selector: "app-home",
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
-
   public cycles$: Observable<any[]>;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.cycles$ = this.dataService.cycles$;
+  }
+
+  countCards(scenario: any): number {
+    let cardAmount = scenario.activeLocation ? 1 : 0;
+    cardAmount += scenario.discardPile.length;
+    cardAmount += scenario.encounterDeck.length;
+    cardAmount += scenario.stagingArea.length;
+    console.log(scenario.title, cardAmount, scenario);
+    return cardAmount;
   }
 
   trackByFn(index: number): number {
